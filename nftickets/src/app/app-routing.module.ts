52adeb0aac7913da939c5f22b5560ec3a91f39f6
ray-blue <surayangobeni882@gmail.com>
2022-02-09 08:48:53 +0200

    Unstoppable Domains Added

---
+++ nftickets/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [{
+  path: "",
+  component: AppComponent,
+},{
+  path: "home",
+  component: HomeComponent
+}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
